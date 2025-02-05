184e3631116c153e975e64b7aba89fd7c0cac533
alex-anie <ocxigin@gmail.com>
2025-02-05 19:11:29 +0100

    only_describe is added

--- cypress/e2e/only_descibe.cy.js
+++ cypress/e2e/only_descibe.cy.js
@@ -1,16 +1,16 @@
-describe.skip('Skip command in cypress', () => {
-    it('Skipped test: clicked on the Mac Book Air card', () => {
+describe('Select and click the product card', () => {
+    it('clicked on the Mac Book Air card', () => {
       cy.visit('https://ecommerce-playground.lambdatest.io/index.php?route=product/category&path=25')
       cy.get('#mz-product-grid-image-44-212408').click()
     })
-  
+
     it('Locate and interact with the specified card element on the web page', () => {
     cy.visit('https://ecommerce-playground.lambdatest.io/index.php?route=product/category&path=25');
     cy.get('#entry_212409 > div > div.col-sm-6.text-left > ul > li:nth-child(7) > a').click();
     cy.get('#mz-product-grid-image-100-212408').should('be.visible').click();
     })
-  
-    it('Skipped test: clicked on the Apple Cinema card', () => {
+
+    it('clicked on the Apple Cinema card', () => {
       cy.visit('https://ecommerce-playground.lambdatest.io/index.php?route=product/category&path=25')
       cy.get('#mz-product-grid-image-42-212408').click()
     })
@@ -30,5 +30,16 @@ describe.skip('Skip command in cypress', () => {
         cy.get('#input-newsletter-yes').check({ force: true })
         cy.get('#input-agree').check({ force: true })
         cy.get('#content > form > div > div > input').click()
+    })
+  })
+
+  describe.only('Interact with the Login form', ()=>{
+    it('Provide users email and password and login', ()=>{
+        cy.visit('https://ecommerce-playground.lambdatest.io/index.php?route=common/home')
+        cy.get('#widget-navbar-217834 > ul > li:nth-child(6)').trigger('mouseover')
+        cy.get('#widget-navbar-217834 > ul > li:nth-child(6) > ul > li:nth-child(1) > a').should('be.visible').click()
+        cy.get('#input-email').type('ocxigin@gmail.com')
+        cy.get('#input-password').type('mypassword')
+        cy.get('#content > div > div:nth-child(2) > div > div > form > input').click()
     })
   })
