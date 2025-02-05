6778bc3ee4fde1d10c7ee4626d3bc1feb5ae4b2e
alex-anie <ocxigin@gmail.com>
2025-02-06 00:48:25 +0100

    skipOn example for operating system is added

---
+++ cypress/e2e/skipOn_os.cy.js
@@ -0,0 +1,11 @@
+// Perform skip test on chrome
+// run test on all browsers except chrome
+describe('Parent Scope Element', () => {
+    it('Open the specified url and navigate to the third blog and then click on the link', () => {
+        cy.skipOn('windows')
+        cy.visit('https://ecommerce-playground.lambdatest.io/index.php?route=extension/maza/blog/category&path=6');
+        cy.get('#entry_210951 > div > div:nth-child(3) > div > div.caption > a').should('be.visible').click()
+        cy.get('#entry_210900 > nav > ol > li:nth-child(3) > a').should('be.visible').click()
+        cy.get('#entry_210951 > div > div:nth-child(5) > div > div.caption > a').should('be.visible').click()
+    });
+})
