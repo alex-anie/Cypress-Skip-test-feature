4afcb9dec79bac038e62c47879c9fa03e56329f2
alex-anie <ocxigin@gmail.com>
2025-02-06 23:22:33 +0100

    before() method is added

---
+++ cypress/e2e/before.cy.js
@@ -0,0 +1,28 @@
+describe('Verify the Operating system before running', ()=>{
+    
+    before(() => {
+        cy.skipOn('mac').skipOn('firefox')
+      });
+
+    it('click the register button and fill in the input tag and click continue', ()=>{
+        cy.visit('https://ecommerce-playground.lambdatest.io/index.php?route=common/home')
+        cy.get('#widget-navbar-217834 > ul > li:nth-child(6)').trigger('mouseover')
+        cy.get('#widget-navbar-217834 > ul > li:nth-child(6) > ul > li:nth-child(2) > a').should('be.visible').click()
+        cy.get('#input-firstname').type('Alex')
+        cy.get('#input-lastname').type('Anie')
+        cy.get('#input-email').type('ocxigin@gmail.com')
+        cy.get('#input-telephone').type('090-222-222-1222-00')
+        cy.get('#input-password').type('mypassword')
+        cy.get('#input-confirm').type('mypassword')
+        cy.get('#input-newsletter-yes').check({ force: true })
+        cy.get('#input-agree').check({ force: true })
+        cy.get('#content > form > div > div > input').click()
+    })
+
+    it('Open the specified url and navigate to the third blog and then click on the link', () => {
+        cy.visit('https://ecommerce-playground.lambdatest.io/index.php?route=extension/maza/blog/category&path=6');
+        cy.get('#entry_210951 > div > div:nth-child(3) > div > div.caption > a').should('be.visible').click()
+        cy.get('#entry_210900 > nav > ol > li:nth-child(3) > a').should('be.visible').click()
+        cy.get('#entry_210951 > div > div:nth-child(5) > div > div.caption > a').should('be.visible').click()
+    });
+  })
