e220c64ef2cdd948c089c8c4ea998c1a182c6206
alex-anie <ocxigin@gmail.com>
2025-02-07 01:13:03 +0100

    afterEach hook

---
+++ cypress/e2e/afterEach.cy.js
@@ -0,0 +1,22 @@
+describe('Select and click the product card', () => {
+
+    afterEach(() => {
+        cy.onlyOn('mac').onlyOn('chrome')
+    });
+
+    it('clicked on the Mac Book Air card', () => {
+      cy.visit('https://ecommerce-playground.lambdatest.io/index.php?route=product/category&path=25')
+      cy.get('#mz-product-grid-image-44-212408').click()
+    })
+
+    it('Locate and interact with the specified card element on the web page', () => {
+    cy.visit('https://ecommerce-playground.lambdatest.io/index.php?route=product/category&path=25');
+    cy.get('#entry_212409 > div > div.col-sm-6.text-left > ul > li:nth-child(7) > a').click();
+    cy.get('#mz-product-grid-image-100-212408').should('be.visible').click();
+    })
+
+    it('clicked on the Apple Cinema card', () => {
+      cy.visit('https://ecommerce-playground.lambdatest.io/index.php?route=product/category&path=25')
+      cy.get('#mz-product-grid-image-42-212408').click()
+    })
+  })
